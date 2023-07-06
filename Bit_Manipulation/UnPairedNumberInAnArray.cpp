ede19667c496f6d54c83f81871cb51de1623cab2
ANKIT SRIVASTAVA <ankit.stallian@gmail.com>
2023-07-06 19:52:04 +0530

    day 5

--- Bit_Manipulation/UnPairedNumberInAnArray.cpp
+++ Bit_Manipulation/UnPairedNumberInAnArray.cpp
@@ -1,10 +1,11 @@
 /*Array of integer is given in which each number has its pair exist return the value of unpaired number*/
 
 
+
 int Xoring(int arr[]){
     int x = 0;
-    for(auto u : arr){
-        x ^= u;
+    for(int i=0; i<arr.size(); i++){
+        x ^= arr[i];
     }
     return x;
 }
