a8de2ddaa8dc8ed61181f6284a87cef5042ae6f6
ANKIT SRIVASTAVA <ankit.stallian@gmail.com>
2023-08-15 11:43:41 +0530

    day 19

--- Bit_Manipulation/NonRepeatingNumbers.cpp
+++ Bit_Manipulation/NonRepeatingNumbers.cpp
@@ -1,4 +1,4 @@
-Given an array A containing 2*N+2 positive numbers, out of which 2*N numbers exist in pairs whereas the other two number occur 
+/*Given an array A containing 2*N+2 positive numbers, out of which 2*N numbers exist in pairs whereas the other two number occur 
 exactly once and are distinct. Find the other two numbers. Return in increasing order.
 
 Example 1:
@@ -9,7 +9,7 @@ arr[] = {1, 2, 3, 2, 1, 4}
 Output:
 3 4 
 Explanation:
-3 and 4 occur exactly once.
+3 and 4 occur exactly once.*/
 
 
 vector<int> singleNumber(vector<int> nums){
